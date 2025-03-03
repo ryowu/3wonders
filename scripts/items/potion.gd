cd30aa9636e005ec689c83e9f54abc80f2a88f1c
ryowu <ryowu@msn.com>
2025-03-03 20:38:28 +1000

    Add potion and hp recover

--- scripts/items/potion.gd
+++ scripts/items/potion.gd
@@ -1,7 +1,9 @@
 extends Sprite2D
 
+@onready var drink: AudioStreamPlayer2D = $drink
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body.recover_hp(5)
+		body.recover(5)
+		drink.play()
 		queue_free()
